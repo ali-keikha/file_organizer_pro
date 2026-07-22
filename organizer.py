e3a4e398126d8510b63969b1f8c9816e31da5d77
Ali Keikha <ahnd7252@gmail.com>
2026-07-22 14:24:46 +0330

    Improve error handling and rename variables for clarity

--- organizer.py
+++ organizer.py
@@ -62,36 +62,37 @@ FILE_TYPES = {
 
 }
 
-def organize_by_name(addres: str, tag_dict: dict):
+def organize_by_name(address: str, tag_dict: dict):
     report_dict = {}
-    report_list = []
-    for item in os.listdir(addres):
-            file_name, file_tag = os.path.splitext(item)
-            file_addres = os.path.join(addres, item)
+    
+    for item in os.listdir(address):
+     
+            _ , file_tag = os.path.splitext(item)
+            file_address = os.path.join(address, item)
             
             for key in tag_dict:
+                
                 tag_list = tag_dict[key]
-                dirc = os.path.join(addres, key)
+                dirc = os.path.join(address, key)
                 for tag in tag_list:
                     
                     if file_tag.lower() == tag:
+                        if key not in report_dict:
+                            report_dict[key] = []
+
+                        report_dict[key].append(item)
                         if not os.path.exists(dirc):
                             os.mkdir(dirc)
-                            shutil.move(file_addres, dirc)
-                            report_list.append(item)
-                            report_dict[key] = report_list
+                            shutil.move(file_address, dirc)
+                            break
                         else:
-                            shutil.move(file_addres, dirc)
-                            report_list.append(item)
-                            report_dict[key] = report_list
+                            shutil.move(file_address, dirc)
+                            break
                     
+    return report_dict
 
-    for key in report_dict:
-        file_count = len(report_dict[key])
-        return (f"{key} Count is {file_count}")
-
-def organize_by_date(folder_addres: str):
-    filtered_directory = Path(folder_addres)
+def organize_by_date(folder_address: str):
+    filtered_directory = Path(folder_address)
     
     for item in filtered_directory.iterdir():
         if not item.is_file():
@@ -99,67 +100,83 @@ def organize_by_date(folder_addres: str):
         info = item.stat()
         date = datetime.fromtimestamp(info.st_ctime)
         folder_name = date.strftime("%Y-%m")
-        file_directory = os.path.join(folder_addres, folder_name)
-        item_addres = str(item)
+        file_directory = os.path.join(folder_address, folder_name)
+        item_address = str(item)
         if not os.path.exists(file_directory):
             os.mkdir(file_directory)
-            shutil.move(item_addres, file_directory)
+            shutil.move(item_address, file_directory)
         else:
-            shutil.move(item_addres, file_directory)
+            shutil.move(item_address, file_directory)
 
-def organize_by_size(file_addres: str):
-    for item in os.listdir(file_addres):
-        item_addres = os.path.join(file_addres, item)
-        if os.path.isdir(item_addres):
+def organize_by_size(file_address: str):
+    for item in os.listdir(file_address):
+        item_address = os.path.join(file_address, item)
+        if os.path.isdir(item_address):
             continue
-        file_size = os.path.getsize(item_addres)
+        file_size = os.path.getsize(item_address)
         
         file_size = file_size / (1024 * 1024)
         if file_size <= 10:
-            dir_addres = os.path.join(file_addres, "small")
-            if not os.path.exists(dir_addres):
-                os.mkdir(dir_addres)
-                shutil.move(item_addres, dir_addres)
+            dir_address = os.path.join(file_address, "small")
+            if not os.path.exists(dir_address):
+                os.mkdir(dir_address)
+                shutil.move(item_address, dir_address)
             else:
-                shutil.move(item_addres, dir_addres)
+                shutil.move(item_address, dir_address)
         
         elif 10 < file_size <= 500:
-            dir_addres = os.path.join(file_addres, "medium")
-            if not os.path.exists(dir_addres):
-                os.mkdir(dir_addres)
-                shutil.move(item_addres, dir_addres)
+            dir_address = os.path.join(file_address, "medium")
+            if not os.path.exists(dir_address):
+                os.mkdir(dir_address)
+                shutil.move(item_address, dir_address)
             else:
-                shutil.move(item_addres, dir_addres)
+                shutil.move(item_address, dir_address)
         
         elif 500 < file_size <= 10000:
-            dir_addres = os.path.join(file_addres, "large")
-            if not os.path.exists(dir_addres):
-                os.mkdir(dir_addres)
-                shutil.move(item_addres, dir_addres)
+            dir_address = os.path.join(file_address, "large")
+            if not os.path.exists(dir_address):
+                os.mkdir(dir_address)
+                shutil.move(item_address, dir_address)
             else:
-                shutil.move(item_addres, dir_addres)
+                shutil.move(item_address, dir_address)
 
 
-
+favorite_folder = ""
 
 while True:
 
-    directory = input("enter directory addres:(for favorite addres type f) \n").lower()
+    directory = input("enter directory address:(for favorite address type f) \n").lower()
 
     if directory == "f":
         if os.path.exists(favorite_folder):
             
             if os.path.isdir(favorite_folder):
             
-                option = int(input("Select Option:\n"
+                while True:
+                    try:
+                        option = int(input("Select Option:\n"
                                 "1) File Organize by Type\n"
                                 "2) File Organize by Date\n"
                                 "3) File Organize by Size\n"
                                 "4) Exit\n"))
+                        
+                        if not 1 <= option <= 4:
+                            print("Please enter number between 1 and 4")
+                            continue
+
+                        break
+
+                    except ValueError:
+                        print("Please enter valid number")
                                 
 
                 if option == 1:
-                    print(organize_by_name(favorite_folder, FILE_TYPES))
+                    final_dict = organize_by_name(favorite_folder, FILE_TYPES)
+                    
+                    for key in final_dict:
+                        length = len(final_dict[key])
+                        print(f"{key} : {length}")
+
                 elif option == 2:
                     organize_by_date(favorite_folder)
                 elif option == 3:
@@ -168,10 +185,10 @@ while True:
                     break
     
                 else:
-                    print("Ente correct number")
+                    print("Please enter a valid option")
 
             else:
-                print("addres is file addres")
+                print("The selected path is a file, not a directory")
                 user_answer = input("try again?y/n")
                 if user_answer.lower() == "n":
                     break
@@ -188,27 +205,41 @@ while True:
             
             if os.path.isdir(directory):
             
-                option = int(input("Select Option:\n"
+                while True:
+                    try:
+                        option = int(input("Select Option:\n"
                                 "1) File Organize by Type\n"
                                 "2) File Organize by Date\n"
                                 "3) File Organize by Size\n"
                                 "4) Add Favorite Folder\n"
                                 "5) Exit\n"))
+                        
+                        if not 1 <= option <= 5:
+                            print("Enter number between 1 and 5")
+                            continue
+
+                        break
+
+                    except ValueError:
+                        print("Please enter valid number")
 
                 if option == 1:
-                    print(organize_by_name(directory, FILE_TYPES))
+                    final_dict = organize_by_name(directory, FILE_TYPES)
+
+                    for key in final_dict:
+                        length = len(final_dict[key])
+                        print(f"{key} : {length}")
+
                 elif option == 2:
                     organize_by_date(directory)
                 elif option == 3:
                     organize_by_size(directory)
                 elif option == 4:
                     favorite_folder = directory
-    
                 else:
-                    print("Ente correct number")
-
+                    break
             else:
-                print("addres is file addres")
+                print("The selected path is a file, not a directory")
                 user_answer = input("try again?y/n")
                 if user_answer.lower() == "n":
                     break
